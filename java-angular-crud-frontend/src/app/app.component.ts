32f7d29c0c03984be22c937e4918200bc3d0b79f
Antonis Sykoutris <antonis.sykoutris@protonmail.com>
2023-04-15 23:54:33 +0300

    Removed test files

--- java-angular-crud-frontend/src/app/app.component.ts
+++ java-angular-crud-frontend/src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'Java Angular Project';
-}
+export class AppComponent {}
